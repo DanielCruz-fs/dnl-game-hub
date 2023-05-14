124b6dbdd0b0d4935a7edc0702a8ddc03406efd8
danny-fs <dannyfsociety@gmail.com>
2023-05-14 02:11:04 -0400

    :tada: Optimize images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from '../hooks/useGames';
 import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
+import getCroppedImageUrl from '../services/image-url';
 
 interface IProps {
     game: Game;
@@ -11,7 +12,7 @@ interface IProps {
 const GameCard = ({ game }: IProps) => {
     return (
         <Card borderRadius={10} overflow={'hidden'}>
-            <Image src={game.background_image}></Image>
+            <Image src={getCroppedImageUrl(game.background_image)}></Image>
             <CardBody>
                 <Heading fontSize={'2xl'}>{game.name}</Heading>
 
