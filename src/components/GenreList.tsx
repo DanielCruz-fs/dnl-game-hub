6e7c2542cc14c2db614d703943ed07bda2051303
danny-fs <dannyfsociety@gmail.com>
2023-05-23 00:50:24 -0400

    :bug: Clean up genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
     Button,
     HStack,
+    Heading,
     Image,
     List,
     ListItem,
@@ -23,29 +24,39 @@ const GenreList = ({ onSelectGenre, selectedGenre }: IProps) => {
     if (isLoading) return <Spinner />;
 
     return (
-        <List>
-            {data.map((g) => (
-                <ListItem key={g.id} paddingY='5px'>
-                    <HStack>
-                        <Image
-                            boxSize='32px'
-                            borderRadius={8}
-                            src={getCroppedImageUrl(g.image_background)}
-                        ></Image>
-                        <Button
-                            fontWeight={
-                                g.id === selectedGenre?.id ? 'bold' : 'normal'
-                            }
-                            fontSize='lg'
-                            variant='link'
-                            onClick={() => onSelectGenre(g)}
-                        >
-                            {g.name}
-                        </Button>
-                    </HStack>
-                </ListItem>
-            ))}
-        </List>
+        <>
+            <Heading fontSize='2xl' marginBottom={3}>
+                Genres
+            </Heading>
+            <List>
+                {data.map((g) => (
+                    <ListItem key={g.id} paddingY='5px'>
+                        <HStack>
+                            <Image
+                                objectFit='cover'
+                                boxSize='32px'
+                                borderRadius={8}
+                                src={getCroppedImageUrl(g.image_background)}
+                            ></Image>
+                            <Button
+                                whiteSpace='normal'
+                                textAlign='left'
+                                fontWeight={
+                                    g.id === selectedGenre?.id
+                                        ? 'bold'
+                                        : 'normal'
+                                }
+                                fontSize='lg'
+                                variant='link'
+                                onClick={() => onSelectGenre(g)}
+                            >
+                                {g.name}
+                            </Button>
+                        </HStack>
+                    </ListItem>
+                ))}
+            </List>
+        </>
     );
 };
 
